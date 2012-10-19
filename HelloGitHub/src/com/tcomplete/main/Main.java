994d01f6dab91767c4990a179b442b53d96dcc0b
ameerirsh <irshad.vit@gmail.com>
2012-10-19 12:43:13 +0100

    First Commit in tcomplete
    This commit is to check whether github is up and running
    
    Project:tcomplete
    Sprint0
    Task:First Commit in tcomplete
    
    Change-Id: Ie769175541afe0b1e02078377911c6bc77513ef9

---
+++ HelloGitHub/src/com/tcomplete/main/Main.java
@@ -0,0 +1,15 @@
+package com.tcomplete.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is to get the git setup on github !!");
+		System.out.println("this line was added from eclipse after setup");
+
+	}
+
+}
